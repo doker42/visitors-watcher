7baacbc7ca91b0c9b5f3dbf15e997d1652ffbe34
doker42 <doker42@gmail.com>
2025-04-27 00:56:47 +0300

    fixed provider 1

--- src/VisitorsWatcherServiceProvider.php
+++ src/VisitorsWatcherServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Doker42\VisitorsWatcher\Providers;
+namespace Doker42\VisitorsWatcher;
 
 use Doker42\VisitorsWatcher\Console\Commands\DeleteOldVisitors;
 use Illuminate\Support\ServiceProvider;
@@ -9,20 +9,20 @@ class VisitorsWatcherServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/visitors.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/visitors.php');
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'visitors');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'visitors');
 
         $this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/visitors'),
+            __DIR__ . '/../resources/assets' => public_path('vendor/visitors'),
         ], 'visitors');
 
         $this->publishes([
-            __DIR__.'/../config/visitors.php' => config_path('visitors.php')
+            __DIR__ . '/../config/visitors.php' => config_path('visitors.php')
         ], 'visitors');
 
         $this->publishesMigrations([
-            __DIR__.'/../database/migrations/' => database_path('migrations')
+            __DIR__ . '/../database/migrations/' => database_path('migrations')
         ], 'visitors');
 
         if ($this->app->runningInConsole()) {
